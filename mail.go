bb509aa84c74c9dcac8d48168d2ef3620dd53850
frederic.leist <frederic.leist@dosing-gmbh.de>
2023-08-14 14:29:20 +0200

    initial commit *mailer struct *message struct

---
+++ mail.go
@@ -0,0 +1,94 @@
+package mail
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"net/smtp"
+	"strings"
+)
+
+type Mailer interface {
+	Send() error
+}
+
+type smtpConfig struct {
+	auth     smtp.Auth
+	user     string
+	password string
+	host     string
+	addr     string
+}
+
+type mail struct {
+	message message
+	config  *smtpConfig
+}
+
+func New(user, password, host string) *mail {
+	c := &smtpConfig{
+		user:     user,
+		password: password,
+		host:     host,
+		addr:     fmt.Sprintf("%s:%d", host, 587),
+	}
+	c.auth = smtp.PlainAuth("", c.user, c.password, c.host)
+	m := &mail{
+		config: c,
+	}
+	return m
+}
+func (m *mail) Send() error {
+	return smtp.SendMail(m.config.addr, m.config.auth, m.config.user, m.message.To(), m.writeMessage())
+}
+
+func (m *mail) SetMessage(msg message) *mail {
+	m.message = msg
+	return m
+}
+
+func (m *mail) writeMessage() []byte {
+	msg := m.message
+	buf := bytes.NewBuffer(nil)
+	buf.WriteString("MIME-Version: 1.0\n")
+	withAttachments := len(msg.Attachments()) > 0
+	buf.WriteString(fmt.Sprintf("From: %s\n", msg.From()))
+	buf.WriteString(fmt.Sprintf("To: %s\n", strings.Join(msg.To(), ",")))
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", msg.Subject()))
+	if len(msg.CC()) > 0 {
+		buf.WriteString(fmt.Sprintf("Cc: %s\n", strings.Join(msg.CC(), ",")))
+	}
+
+	if len(msg.BCC()) > 0 {
+		buf.WriteString(fmt.Sprintf("Bcc: %s\n", strings.Join(msg.BCC(), ",")))
+	}
+
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed;\n boundary=%s\n\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+
+	}
+	buf.WriteString(fmt.Sprintf("Content-Type: %s\n\n", http.DetectContentType([]byte(msg.Body()))))
+	buf.WriteString(msg.Body())
+	if withAttachments {
+		for k, v := range msg.Attachments() {
+			buf.WriteString(fmt.Sprintf("\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
+		}
+
+		buf.WriteString("--")
+	}
+
+	return buf.Bytes()
+}
---
+++ message.go
@@ -0,0 +1,131 @@
+package mail
+
+import (
+	"bytes"
+	"html/template"
+	"os"
+	"path/filepath"
+)
+
+type Message interface {
+	FromText(text string) *message
+	FromTemplateString(tpl string) *message
+	FromTemplateFile(file string) *message
+	SetFrom(from string)
+	SetTo(to []string)
+	SetCC(cc []string)
+	SetBCC(bcc []string)
+	SetSubject(s string)
+	Attach(files []string)
+}
+type message struct {
+	from        string
+	to          []string
+	cc          []string
+	bcc         []string
+	subject     string
+	body        string
+	attachments map[string][]byte
+}
+
+func (m *message) SetFrom(from string) {
+	m.from = from
+}
+func (m *message) From() string {
+	return m.from
+}
+
+func (m *message) SetTo(to ...string) {
+	m.to = to
+}
+
+func (m *message) To() []string {
+	return m.to
+}
+
+func (m *message) SetCC(cc ...string) {
+	m.cc = cc
+}
+
+func (m *message) CC() []string {
+	return m.cc
+}
+
+func (m *message) SetBCC(bcc ...string) {
+	m.bcc = bcc
+}
+
+func (m *message) BCC() []string {
+	return m.bcc
+}
+
+func (m *message) SetSubject(subject string) {
+	m.subject = subject
+}
+
+func (m *message) Subject() string {
+	return m.subject
+}
+
+func (m *message) Attach(files []string) error {
+	for _, file := range files {
+		b, err := os.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		_, fileName := filepath.Split(file)
+		m.attachments[fileName] = b
+	}
+	return nil
+}
+
+func (m *message) Attachments() map[string][]byte {
+	return m.attachments
+}
+
+func (m *message) Body() string {
+	return m.body
+}
+
+func FromText(text string) *message {
+	m := new()
+	m.body = text
+	return m
+}
+
+func FromTemplateString(data any, tpl string) (*message, error) {
+	buff := bytes.Buffer{}
+	template, _ := template.New("tinymail").Parse(tpl)
+	err := template.Execute(&buff, data)
+	if err != nil {
+		return nil, err
+	}
+	m := new()
+	m.body = buff.String()
+	return m, nil
+}
+
+func FromTemplateFile(data any, filenames ...string) (*message, error) {
+	buff := bytes.Buffer{}
+	template, _ := template.ParseFiles(filenames...)
+	err := template.Execute(&buff, data)
+	if err != nil {
+		return nil, err
+	}
+	m := new()
+	m.body = buff.String()
+	return m, nil
+}
+
+func new() *message {
+	return &message{
+		from:        "",
+		to:          []string{},
+		cc:          []string{},
+		bcc:         []string{},
+		subject:     "",
+		body:        "",
+		attachments: map[string][]byte{},
+	}
+}
